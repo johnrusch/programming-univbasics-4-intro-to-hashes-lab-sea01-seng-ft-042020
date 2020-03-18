025cba3498040970dc2ab9891d20bb9267b7f1aa
John Rusch <johnrusch7@gmail.com>
2020-03-18 22:54:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,10 @@ def pioneer
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  number = rand(10)
+  id_generator = {
+    id: number
+  }
 end
 
 def my_hash_creator(key, value)
